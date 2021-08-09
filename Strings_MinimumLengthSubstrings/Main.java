2769823553adb4a442bc3e8a4ed84107d8a678c8
Ryan Gonzalez <ryangonzalez@Ryans-MacBook-Air.local>
2021-08-08 22:43:06 -0500

    Added solution for MinimumLengthSubstrings problem

--- Strings_MinimumLengthSubstrings/Main.java
+++ Strings_MinimumLengthSubstrings/Main.java
@@ -8,11 +8,78 @@ import java.util.*;
 class Main {
 
     // Add any helper functions you may need here
+    private Map<Character, Integer> buildMapOfCharactersToOccurences(String s){
+        Map<Character, Integer> map = new HashMap<>();
 
+        for (int i = 0; i < s.length(); i++){
+            int numOfOccurrences = map.getOrDefault(s.charAt(i), 0);
+            map.put(s.charAt(i), numOfOccurrences + 1);
+        }
 
-    int minLengthSubstring(String s, String t) {
+        return map;
+    }
+
+    private void addCharacterToHashTableMapping(Map<Character, Integer> map, Character c){
+        int occurrences = map.getOrDefault(c, 0);
+        map.put(c, occurrences + 1);
+    }
+
+    public int minLengthSubstring(String s, String t) {
         // Write your code here
-        return 0;
+        Map<Character, Integer> reqCharacters = buildMapOfCharactersToOccurences(t);
+        Map<Character, Integer> windowCharacterMapping = new HashMap<>();
+
+        int left = 0;
+        int right = 0;
+
+        int totalCharactersToMatch = reqCharacters.size();
+        int charInWindowThatMatch = 0;
+
+        int minimumWindowLengthSeenSoFar = Integer.MAX_VALUE;
+        String minWindowSubstring = "";
+
+        while (right < s.length()){
+            char characterAtRightPointer = s.charAt(right);
+            addCharacterToHashTableMapping(windowCharacterMapping, characterAtRightPointer);
+
+            boolean rightCharIsRequired = reqCharacters.containsKey(characterAtRightPointer);
+            if (rightCharIsRequired) {
+                boolean requirementForCharacterMet = reqCharacters.get(characterAtRightPointer).intValue()
+                        == windowCharacterMapping.get(characterAtRightPointer).intValue();
+
+                if (requirementForCharacterMet) {
+                    charInWindowThatMatch++;
+                }
+            }
+
+            while (charInWindowThatMatch == totalCharactersToMatch && left <= right){
+                char characterAtLeftPointer = s.charAt(left);
+                int windowSize = right - left + 1;
+
+                if (windowSize < minimumWindowLengthSeenSoFar){
+                    minimumWindowLengthSeenSoFar = windowSize;
+                    minWindowSubstring = s.substring(left, right + 1);
+                }
+
+                windowCharacterMapping.put(characterAtLeftPointer, windowCharacterMapping.get(characterAtLeftPointer) - 1);
+
+                boolean leftCharIsRequired = reqCharacters.containsKey(characterAtLeftPointer);
+                if (leftCharIsRequired){
+                    boolean characterFailsRequirement = windowCharacterMapping.get(characterAtLeftPointer).intValue()
+                            < reqCharacters.get(characterAtLeftPointer).intValue();
+                    if (characterFailsRequirement){
+                        charInWindowThatMatch--;
+                    }
+                }
+                left++;
+            }
+            right++;
+        }
+
+        if (minWindowSubstring.length() == 0){
+            return -1;
+        }
+        return minWindowSubstring.length();
     }
 
     // These are the tests we use to determine if the solution is correct.
@@ -49,6 +116,12 @@ class Main {
         int expected_2 = -1;
         int output_2 = minLengthSubstring(s_2, t_2);
         check(expected_2, output_2);
+
+        String s_3 = "dcbefebceaz";
+        String t_3 = "abc";
+        int expected_3 = 4;
+        int output_3 = minLengthSubstring(s_3, t_3);
+        check(expected_3, output_3);
 
         // Add your own test cases here
 
